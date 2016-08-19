6fe5e04ab3ba41e3ea1212506c70966ac2d019b5
localadmin <localadmin@compozed-tss-03.allstate.com>
2016-08-19 12:20:35 -0700

    added some Payne Train code

--- app.js
+++ app.js
@@ -23,7 +23,23 @@ var dbConfig = {
 }
 sql.connect('mssql://nextgen:nextgen@e3sst003.allstate.com:1433/E3D_ESMDB').then(function() {
   console.log('success');
-  new sql.Request().query("select * from EC_Contact_Table where CT_User_ID = 'rdumiak'").then(function(recordset) {
+  // new sql.Request().query("select * from EC_Contact_Table where CT_User_ID = 'rdumiak'").then(function(recordset) {
+  new sql.Request().query("select * from veom_contact_inclusion where ECI_CCT_Contact_ID = 'rdumiak'").then(function(recordset) {
+    //EOM Tables
+    //select * from cct_contact
+    // select EClass_Name from dbo.ef_select_eom_class_byparent('Application_Class_Infrastructure', 1, 1000) 'ECC_Managed_Object'
+    // select * from vw_iris_select_application_contacts where ECI_CCT_Contact_ID = 'epay0'
+    //select * from vw_iris_select_application_contacts where applicationName like 'CMDB%'
+    // select * from vw_iris_select_application_contacts where ECI_Contact_Type like 'Product-Owner'
+    //  select * from vw_iris_select_business_applications where ECI_Contact_Type like 'Product-Owner'
+    // vw_iris_select_business_applications
+
+// [12:16]  
+// select * from vw_iris_select_application_contacts where ECI_CCT_Contact_ID = 'epay0'
+//
+// select * from vw_iris_select_application_contacts where applicationName like 'CMDB%'
+// and  ECI_Contact_Type IN ('Product-Manager','Product-Support')
+  // new sql.Request().query("select * from Application_Container_Map where Container_UUID = '2D082060-8520-4E44-A97D-0B41679089D9'").then(function(recordset) {
     console.dir(recordset);
   }).catch(function(err) {
     console.log(err);
